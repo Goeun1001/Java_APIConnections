5b2cd72137ab5abb764d46912dabffb1cb68820e
GoEun <gogo8272@gmail.com>
2020-08-20 21:27:57 +0900

    4. RxJava + Retrofit - GET

--- app/src/main/java/com/muradismayilov/api_json_example/MainActivity.java
+++ app/src/main/java/com/muradismayilov/api_json_example/MainActivity.java
@@ -1,143 +1,70 @@
 package com.muradismayilov.api_json_example;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
-import android.widget.Toast;
-
-import com.muradismayilov.api_json_example.pojo.CreateUserResponse;
-import com.muradismayilov.api_json_example.pojo.MultipleResource;
-import com.muradismayilov.api_json_example.pojo.User;
-import com.muradismayilov.api_json_example.pojo.UserList;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import rx.Observer;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView responseText;
-    APIInterface apiInterface;
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private GitHubRepoAdapter adapter = new GitHubRepoAdapter();
+    private Subscription subscription;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        responseText = (TextView) findViewById(R.id.responseText);
-        apiInterface = APIClient.getClient().create(APIInterface.class);
 
-        /**
-         GET List Resources
-         **/
-        Call<MultipleResource> call = apiInterface.doGetListResources();
-        call.enqueue(new Callback<MultipleResource>() {
-            @Override
-            public void onResponse(Call<MultipleResource> call, Response<MultipleResource> response) {
+        final ListView listView = (ListView) findViewById(R.id.list_view_repos);
+        listView.setAdapter(adapter);
 
-
-                Log.d("TAG",response.code()+"");
-
-                String displayResponse = "";
-
-                MultipleResource resource = response.body();
-                Integer text = resource.page;
-                Integer total = resource.total;
-                Integer totalPages = resource.totalPages;
-                List<MultipleResource.Datum> datumList = resource.data;
-
-                displayResponse += text + " Page\n" + total + " Total\n" + totalPages + " Total Pages\n";
-
-                for (MultipleResource.Datum datum : datumList) {
-                    displayResponse += datum.id + " " + datum.name + " " + datum.pantoneValue + " " + datum.year + "\n";
+        final EditText editTextUsername = (EditText) findViewById(R.id.edit_text_username);
+        final Button buttonSearch = (Button) findViewById(R.id.button_search);
+        buttonSearch.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                final String username = editTextUsername.getText().toString();
+                if (!TextUtils.isEmpty(username)) {
+                    getStarredRepos(username);
                 }
-
-                responseText.setText(displayResponse);
-
-            }
-
-            @Override
-            public void onFailure(Call<MultipleResource> call, Throwable t) {
-                call.cancel();
             }
         });
+    }
 
-        /**
-         Create new user
-         **/
-        User user = new User("morpheus", "leader");
-        Call<User> call1 = apiInterface.createUser(user);
-        call1.enqueue(new Callback<User>() {
-            @Override
-            public void onResponse(Call<User> call, Response<User> response) {
-                User user1 = response.body();
+    @Override protected void onDestroy() {
+        if (subscription != null && !subscription.isUnsubscribed()) {
+            subscription.unsubscribe();
+        }
+        super.onDestroy();
+    }
 
-                Toast.makeText(getApplicationContext(), user1.name + " " + user1.job + " " + user1.id + " " + user1.createdAt, Toast.LENGTH_SHORT).show();
+    private void getStarredRepos(String username) {
+        subscription = GitHubClient.getInstance()
+                .getStarredRepos(username)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<List<GitHubRepo>>() {
+                    @Override public void onCompleted() {
+                        Log.d(TAG, "In onCompleted()");
+                    }
 
-            }
+                    @Override public void onError(Throwable e) {
+                        e.printStackTrace();
+                        Log.d(TAG, "In onError()");
+                    }
 
-            @Override
-            public void onFailure(Call<User> call, Throwable t) {
-                call.cancel();
-            }
-        });
-
-        /**
-         GET List Users
-         **/
-        Call<UserList> call2 = apiInterface.doGetUserList("2");
-        call2.enqueue(new Callback<UserList>() {
-            @Override
-            public void onResponse(Call<UserList> call, Response<UserList> response) {
-
-                UserList userList = response.body();
-                Integer text = userList.page;
-                Integer total = userList.total;
-                Integer totalPages = userList.totalPages;
-                List<UserList.Datum> datumList = userList.data;
-                Toast.makeText(getApplicationContext(), text + " page\n" + total + " total\n" + totalPages + " totalPages\n", Toast.LENGTH_SHORT).show();
-
-                for (UserList.Datum datum : datumList) {
-                    Toast.makeText(getApplicationContext(), "id : " + datum.id + " name: " + datum.first_name + " " + datum.last_name + " avatar: " + datum.avatar, Toast.LENGTH_SHORT).show();
-                }
-
-
-            }
-
-            @Override
-            public void onFailure(Call<UserList> call, Throwable t) {
-                call.cancel();
-            }
-        });
-
-
-        /**
-         POST name and job Url encoded.
-         **/
-        Call<UserList> call3 = apiInterface.doCreateUserWithField("morpheus","leader");
-        call3.enqueue(new Callback<UserList>() {
-            @Override
-            public void onResponse(Call<UserList> call, Response<UserList> response) {
-                UserList userList = response.body();
-                Integer text = userList.page;
-                Integer total = userList.total;
-                Integer totalPages = userList.totalPages;
-                List<UserList.Datum> datumList = userList.data;
-                Toast.makeText(getApplicationContext(), text + " page\n" + total + " total\n" + totalPages + " totalPages\n", Toast.LENGTH_SHORT).show();
-
-                for (UserList.Datum datum : datumList) {
-                    Toast.makeText(getApplicationContext(), "id : " + datum.id + " name: " + datum.first_name + " " + datum.last_name + " avatar: " + datum.avatar, Toast.LENGTH_SHORT).show();
-                }
-
-            }
-
-            @Override
-            public void onFailure(Call<UserList> call, Throwable t) {
-                call.cancel();
-            }
-        });
-
+                    @Override public void onNext(List<GitHubRepo> gitHubRepos) {
+                        Log.d(TAG, "In onNext()");
+                        adapter.setGitHubRepos(gitHubRepos);
+                    }
+                });
     }
 }
